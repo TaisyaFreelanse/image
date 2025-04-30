a0627fc6b7be472955f63d041127800fa6875bd8
Taisiya <smilikgirl1@gmail.com>
2025-04-30 13:20:54 +0300

    добавила правки в vite.config.js и другие изменения

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',  // Это откроет сервер для всех устройств в сети
-    port: 5173,        // Убедись, что порт совпадает с твоим
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    allowedHosts: ['dev.phototune.ai', 'www.dev.phototune.ai'],
   },
 });
+
